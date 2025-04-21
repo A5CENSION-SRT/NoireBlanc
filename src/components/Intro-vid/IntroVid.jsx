b774ffea67194edf50132c5f7de9f5a25114c491
snehal <snehalreddy@gmail.com>
2025-04-21 19:48:31 +0530

    video commit and intitaliztion of sort and filter component

--- src/components/Intro-vid/IntroVid.jsx
+++ src/components/Intro-vid/IntroVid.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import './IntroVid.css'
 const IntroVid = () => {
   return (
     <div>
       <div className='vid-container'>
-        <video className='intro-vid' autoPlay loop muted>
-          <source src="/assets/videos/introvid.mp4" type="video/mp4" />
+        <video className='intro-vid' autoplay loop muted>
+          <source src="/videos/introvid.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
+        <div className='overlay'>
+        <div className="overlay-text">power.</div>
+        <div className="overlay-text">precision.</div>
+        <div className="overlay-text">prestige.</div>
+        </div>
       </div>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import Navbar from "../components/Navbar/Navbar";
-
+import IntroVid from '../components/Intro-vid/IntroVid';
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <div className='vid-container'>
-        <video className='intro-vid' autoPlay loop muted>
-          <source src="/assets/videos/introvid.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
+      <IntroVid />
     </div>
   )
 }
