61cddabd619cd4a5b8e3e357e2b79c21134da394
snehal <snehalreddy@gmail.com>
2025-04-30 18:24:26 +0530

    commit

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
 import SortFilter from '../components/SortFilter/SortFilter'
-
+import AllProductCards from '../components/ProductCards/AllProductCards'
 const Products = () => {
   return (
     <div>
       <Navbar/>
       <SortFilter/>
+      <AllProductCards/>
+
+      
+
       
     </div>
   )
